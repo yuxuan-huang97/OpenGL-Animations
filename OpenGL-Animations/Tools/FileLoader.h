8134505ca7fcd7e2f3a08de93e095610c5cd3ebc
Y.X. Huang <34802897+DragonHuang97@users.noreply.github.com>
2020-07-15 23:03:24 +0800

    Update FileLoader.h

--- OpenGL-Animations/Tools/FileLoader.h
+++ OpenGL-Animations/Tools/FileLoader.h
@@ -1,15 +1,21 @@
-// An obj loader that assists the loading process of obj models into OpenGL
+// An file loader to handle the loading process of shaders and obj models into OpenGL
 // written by Yuxuan Huang
 
 #include <vector>
 #include <string>
+#include <iostream>
+#include <fstream>
+#include "../../glad/glad.h"
 #include "../../glm/glm.hpp"
 #include "../../glm/gtc/matrix_transform.hpp"
 #include "../../glm/gtc/type_ptr.hpp"
 
 using namespace std;
 
-bool loadobj(const char* path, std::vector < float >& out_vertices,
+string readFile(const char* filePath);
+
+void loadShader(GLuint shaderID, const char* filepath);
+
+void loadobj(const char* path, std::vector < float >& out_vertices,
     std::vector < float >& out_uvs,
     std::vector < float >& out_normals);
-
