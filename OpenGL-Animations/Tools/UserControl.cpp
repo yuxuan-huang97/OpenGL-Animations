80aab8842ff3b25af31fe92f5ad6c1d2bb4a1f95
Y.X. Huang <34802897+DragonHuang97@users.noreply.github.com>
2020-07-27 15:38:12 +0800

    New camera movement script

--- OpenGL-Animations/Tools/UserControl.cpp
+++ OpenGL-Animations/Tools/UserControl.cpp
@@ -2,7 +2,7 @@
 
 
 void move_obj(SDL_Event event, glm::vec3& pos, glm::vec3 view, glm::vec3 up, float speed) {
-    //float speed = 0.2;
+    
     glm::vec3 translation;
 
     view = glm::normalize(view);
@@ -22,4 +22,125 @@ void move_obj(SDL_Event event, glm::vec3& pos, glm::vec3 view, glm::vec3 up, flo
     }
     translation *= speed;
     pos += translation;
+}
+
+void move_camera(SDL_Event event, glm::vec3& pos, glm::vec3& lookat, glm::vec3 up, float rotspeed, float movspeed) {
+
+    // vector preprocessing
+    glm::vec3 view = lookat - pos;
+    view = glm::normalize(view);
+    up = glm::normalize(up);
+    glm::vec3 left = glm::cross(up, view);
+
+    // mouse movement event for camera rotation
+    if (event.type == SDL_MOUSEMOTION) {
+        float rotX = 0;
+        float rotY = 0;
+
+        // obtain mouse movement data and convert to radians
+        rotX += event.motion.xrel;
+        rotY += event.motion.yrel;
+        rotX = rotX * M_PI / 180.0f;
+        rotY = rotY * M_PI / 180.0f;
+
+        rotate(rotX, rotY, rotspeed, view);
+
+        //printf("X: %i, Y: %i \n", event.motion.x, event.motion.y);
+    }
+
+    lookat = pos + view;
+
+    // keyboard event for camera movement
+    bool forward = false, backward = false, leftward = false, rightward = false, upward = false, downward = false;
+    if (event.type == SDL_KEYDOWN) {
+        switch (event.key.keysym.sym) {
+        case SDLK_w: forward = true; break;
+        case SDLK_a: leftward = true; break;
+        case SDLK_s: backward = true; break;
+        case SDLK_d: rightward = true; break;
+        case SDLK_e: upward = true; break;
+        case SDLK_c: downward = true; break;
+        default: break;
+        }
+    }
+    
+    else if (event.type == SDL_KEYUP) {
+        switch (event.key.keysym.sym) {
+        case SDLK_w: forward = false; break;
+        case SDLK_a: leftward = false; break;
+        case SDLK_s: backward = false; break;
+        case SDLK_d: rightward = false; break;
+        case SDLK_e: upward = false; break;
+        case SDLK_c: downward = false; break;
+        default: break;
+        }
+    }
+
+    if (forward) {
+        pos += view * movspeed;
+        lookat += view * movspeed;
+    }
+    else if (backward) {
+        pos -= view * movspeed;
+        lookat -= view * movspeed;
+    }
+
+    if (leftward) {
+        pos += left * movspeed;
+        lookat += left * movspeed;
+    }
+    else if (rightward) {
+        pos -= left * movspeed;
+        lookat -= left * movspeed;
+    }
+
+    if (upward) {
+        pos += up * movspeed;
+        lookat += up * movspeed;
+    }
+    else if (downward) {
+        pos -= up * movspeed;
+        lookat -= up * movspeed;
+    }
+}
+
+void bound_rotate(SDL_Window* window, glm::vec3 pos, glm::vec3& lookat) {
+    
+    glm::vec3 view = lookat - pos;
+
+    int x, y, w, h;
+    float rotX = 0;
+    float rotY = 0;
+    float speed = 0.005;
+    int tolerance = 10;
+    SDL_GetMouseState(&x, &y);
+    SDL_GetWindowSize(window, &w, &h);
+
+    if (x <= tolerance) rotX = -1.0f;
+    else if (x >= w - tolerance - 1) rotX = 1.0f;
+    if (y <= tolerance) rotY = 1.0f;
+    else if (y >= h - tolerance - 1) rotY = -1.0f;
+    rotate(rotX, rotY, speed, view);
+
+    lookat = pos + view;
+}
+
+void rotate(float rotX, float rotY, float speed, glm::vec3& target) {
+    // convert to spherical coordinate
+    float theta = atan2(target.y, target.x); // -PI - +PI
+    float phi = acos(target.z); // 0 - PI
+
+    // rotate in spherical coordinate
+    theta -= speed * rotX;
+    phi += speed * rotY;
+
+    if (theta < -M_PI) theta += 2 * M_PI;
+    else if (theta > M_PI) theta -= 2 * M_PI;
+    if (phi < 0) phi = 0;
+    else if (phi > M_PI) phi = M_PI;
+
+    // convert back to cartesian coordinate
+    target.x = cos(theta) * sin(phi);
+    target.y = sin(theta) * sin(phi);
+    target.z = cos(phi);
 }
--- OpenGL-Animations/Tools/UserControl.h
+++ OpenGL-Animations/Tools/UserControl.h
@@ -4,6 +4,7 @@
 
 #include <SDL.h>
 #include <SDL_opengl.h>
+#include <math.h>
 
 #define GLM_FORCE_RADIANS
 #include "../../glm/glm.hpp"
@@ -15,3 +16,9 @@ using namespace std;
 void move_obj(SDL_Event event, glm::vec3& pos, glm::vec3 view, glm::vec3 up, float speed); 
 // pos is the position of the object to be moved
 // view is the vector pointing from eye to focus
+
+void move_camera(SDL_Event event, glm::vec3& pos, glm::vec3& lookat, glm::vec3 up, float rotspeed, float movspeed);
+
+void bound_rotate(SDL_Window* window, glm::vec3 pos, glm::vec3& lookat);
+
+void rotate(float rotX, float rotY, float speed, glm::vec3& target);
